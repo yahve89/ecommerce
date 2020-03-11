2fd185cb0ea66b780d936a2744e2395367ea8632
Ivan Alexandrov <ivan-alexandrov@inbox.ru>
2020-03-11 16:42:43 +0400

    настройка БД для хостинга

--- server/config/db.php
+++ server/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=ecommerce',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=alexon34_ecommerce',
+    'username' => '045737079_ecom',
+    'password' => 'v5X+e+%:\k68',
     'charset' => 'utf8',
 ];
